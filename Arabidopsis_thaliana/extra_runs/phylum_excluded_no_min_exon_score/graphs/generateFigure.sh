0a47be9f8a52dfbeaa3e9d9022e5cbcdd9aa189b
Tomas Bruna <bruna.tomas@gmail.com>
2019-10-25 15:29:52 -0400

    Exon score vs IBA score Sn-Sp curves for introns, Athal phylum excluded
    Figure generating scripts and result itself

---
+++ Arabidopsis_thaliana/extra_runs/phylum_excluded_no_min_exon_score/graphs/generateFigure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Tomas Bruna
+# Generate data for Sn-Sp curve showing effect of changing threshold for
+# exon score and intron borders alignment score with raw Spaln output.
+
+annot=$(dirname $0)/../../../annot/annot.gtf
+pseudo=$(dirname $0)/../../../annot/pseudo.gff3
+input=$(dirname $0)/introns.gff
+binFolder=$(dirname $0)/../../../../bin
+
+grep -P "\t[Ii]ntron\t" $(dirname $0)/../Spaln/spaln.gff > $input
+
+# This script is terribly inefficient which is fine -- it is only used here
+$(dirname $0)/generateROCForExons.sh $input $annot eScore.csv $pseudo
+
+$binFolder/filter_gff.sh $input 25 LeScore > y
+$binFolder/filter_gff.sh y 25 ReScore > eScore_25.gff
+
+$binFolder/generateROC.sh eScore_25.gff $annot $pseudo eScore_25_al_score.csv 0 1 0.01 al_score
+$binFolder/generateROC.sh $input $annot $pseudo al_score.csv 0 1 0.01 al_score
+
+rm y
+
+# Cut off tails with little data
+head -54 al_score.csv > tmp; mv tmp al_score.csv
+head -60 eScore_25_al_score.csv > tmp; mv tmp eScore_25_al_score.csv
+head -34 eScore.csv > tmp; mv tmp eScore.csv
+
+# Create data for combined curve
+cat <(head -11 eScore.csv) <(tail -n+2 eScore_25_al_score.csv) > tmp; mv tmp eScore_25_al_score.csv
+
+# Plot
+gnuplot plotCurves.gp
---
+++ Arabidopsis_thaliana/extra_runs/phylum_excluded_no_min_exon_score/graphs/plotCurves.gp
@@ -0,0 +1,35 @@
+#!/usr/bin/env gnuplot
+#
+# Tomas Bruna
+
+set datafile separator ','
+unset key
+set xlabel "Specificity"
+set ylabel "Sensitivity"
+
+set mxtics 2
+set mytics 2
+
+set grid xtics ytics mxtics mytics lt 0 lw 1, lt rgb "#bbbbbb" lw 1
+
+set term pdf
+set output "curves.pdf"
+
+set xtics 0,10,100
+set ytics 0,10,100
+set mxtics 2
+set mytics 2
+
+set xrange [0:100]
+set yrange [0:100]
+
+set style data l
+set size ratio -1
+
+width = 2
+
+plot "eScore_25_al_score.csv" using 3:2 lw width lt rgb 'red', \
+     "eScore.csv" using 3:2 dt 2 lw width lt rgb '#ff7100', \
+     "al_score.csv" using 3:2 dt 2 lw width lt rgb '#812581', \
+     "eScore_25_al_score.csv" using 3:($1 == 0.1 ? $2 : 1/0) title "" w p pt 2 lw 3 ps 0.75 lt rgb "black", \
+     "eScore_25_al_score.csv" using 3:($1 == 25 ? $2 : 1/0) title "" w p pt 6 lw 3 ps 0.7 lt rgb "black"
