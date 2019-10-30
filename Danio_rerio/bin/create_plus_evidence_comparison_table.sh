7ac8a4db40c64c2a3e8145569ca6a579277a8a2b
Tomas Bruna <bruna.tomas@gmail.com>
2019-10-30 14:31:47 -0400

    Modify evidence comparison script for D. rerio
    Gene Sn is computed against a set of complete genes.

--- Danio_rerio/bin/create_plus_evidence_comparison_table.sh
+++ Danio_rerio/bin/create_plus_evidence_comparison_table.sh
@@ -7,14 +7,24 @@
 #
 # This script needs to be run from species folder in which the results
 # of EP+ with different evidences are already generated.
+#
+# This is a modification of the general script: Gene sensitivity
+# is computed against the set of complete genes.
 # ==============================================================
 
+
+getGeneColumn() {
+    prediction="$1/genemark.gtf"
+    "$(dirname $0)/../../bin/compare_intervals_exact.pl" --f1 annot/completeGenes.gtf \
+        --f2 $prediction --gene --pseudo annot/pseudo.gff3 | head -2 | tail -1 | cut -f4 | tr "\n" "/"
+    "$(dirname $0)/../../bin/compare_intervals_exact.pl" --f1 annot/annot.gtf \
+        --f2 $prediction --gene --pseudo annot/pseudo.gff3 | head -3 | tail -1 | cut -f4
+}
 
 getColumn() {
     header=$1
     prediction="$2/genemark.gtf"
-    echo "$header"
-    "$(dirname $0)/compute_accuracies.sh" annot/annot.gtf \
+    "$(dirname $0)/../../bin/compute_accuracies.sh" annot/annot.gtf \
         annot/pseudo.gff3 "$prediction" $types | cut -f2 | \
         awk '{if (NR%2 == 1) {printf "%.2f/", $0} else {printf "%.2f\n", $0}}'
 }
@@ -34,10 +44,25 @@ prothintFolder="$1"
 
 echo $prothintFolder
 
-types="gene cds initial internal terminal single intron start stop"
+# Handle genes separately
 
+echo -en "-----------\tES\tEP\tEP+_introns\tES+_start_stops\tEP+\n"
 
-table=$(echo -e "-----------\nGene_Sn/Sp----\nExon_Sn/Sp----\nInitial_Sn/Sp-\
+table="Gene_Sn/Sp----"
+
+appendColumn "$(getGeneColumn "ES")"
+appendColumn "$(getGeneColumn "$prothintFolder/EP/train/")"
+appendColumn "$(getGeneColumn "$prothintFolder/EP/plus_introns_only/")"
+appendColumn "$(getGeneColumn "$prothintFolder/EP/plus_starts_stops_only/")"
+appendColumn "$(getGeneColumn "$prothintFolder/EP/plus/")"
+
+echo "$table"
+
+# The rest is normal
+
+types="cds initial internal terminal single intron start stop"
+
+table=$(echo -e "Exon_Sn/Sp----\nInitial_Sn/Sp-\
 \nInternal_Sn/Sp\nTerminal_Sn/Sp\nSingle_Sn/Sp--\nIntron_Sn/Sp--\nStart_Sn/Sp---\
 \nStop_Sn/Sp----")
 
