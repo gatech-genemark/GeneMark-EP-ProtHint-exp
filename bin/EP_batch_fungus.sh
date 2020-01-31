4bd8f3ec4cc4dae67c1d1968713962139599f058
Tomas Bruna <bruna.tomas@gmail.com>
2020-01-31 17:48:32 -0500

    Scripts for running EP/EP+ for all exclusion levels

---
+++ bin/EP_batch_fungus.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Tomas Bruna
+#
+# Run a batch of EPs (train only and plus) for the list of specified
+# ProtHint folders.
+#
+# The genome is assumed to be in data/genome.fasta.masked
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 prothint1 prothint2 ..."
+    exit
+fi
+
+binDir=$(dirname "$0")
+genome=$(readlink -e "data/genome.fasta.masked")
+
+GMES=$(readlink -e "${binDir}/GeneMarkES/bin/gmes_petap.pl")
+
+EP_ARGS="--verbose --fungus \
+    --seq $genome \
+    --cores=8 --soft_mask 0 \
+    --EP ../../prothint.gff"
+
+runEP() {
+    cd "$1"
+    mkdir EP; cd EP
+    mkdir train plus
+
+    cd train
+    $GMES $EP_ARGS > log
+    cd ..
+
+    cd plus
+    $GMES $EP_ARGS --evidence ../../evidence.gff > log
+    cd ..
+
+    cd ..; cd ..
+}
+
+
+for folder in "$@"
+do
+    runEP "$folder"
+done
