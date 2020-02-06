29369694273e77e6a26a63a6422b9c16f6b4c277
Tomas Bruna <bruna.tomas@gmail.com>
2020-02-06 18:34:06 -0500

    Update gmes path

--- Drosophila_melanogaster/extra_runs/ep_with_fully_supported_genes/run_genemark.sh
+++ Drosophila_melanogaster/extra_runs/ep_with_fully_supported_genes/run_genemark.sh
@@ -8,15 +8,15 @@ exclusionLevel() {
     mkdir $level 2> /dev/null
     cd $level
     mkdir ep_full_genes; cd ep_full_genes
-    ../../../../../bin/GeneMarkES/bin/gmes_petap.pl --verbose --seq ../../../../data/genome.fasta.masked --cores=8 --soft_mask auto --EP ../introns_full_support.gff --evidence ../introns_full_support.gff
+    ../../../../../bin/gmes/gmes_petap.pl --verbose --seq ../../../../data/genome.fasta.masked --cores=8 --soft_mask auto --EP ../introns_full_support.gff --evidence ../introns_full_support.gff
     cd ..
 
     mkdir ep_all; cd ep_all
-    ../../../../../bin/GeneMarkES/bin/gmes_petap.pl --verbose --seq ../../../../data/genome.fasta.masked --cores=8 --soft_mask auto --EP ../hc_introns.gff --evidence ../hc_introns.gff
+    ../../../../../bin/gmes/gmes_petap.pl --verbose --seq ../../../../data/genome.fasta.masked --cores=8 --soft_mask auto --EP ../hc_introns.gff --evidence ../hc_introns.gff
     cd ..
 
     mkdir ep_all_true; cd ep_all_true
-    ../../../../../bin/GeneMarkES/bin/gmes_petap.pl --verbose --seq ../../../../data/genome.fasta.masked --cores=8 --soft_mask auto --EP ../hc_introns_true.gff --evidence ../hc_introns_true.gff
+    ../../../../../bin/gmes/gmes_petap.pl --verbose --seq ../../../../data/genome.fasta.masked --cores=8 --soft_mask auto --EP ../hc_introns_true.gff --evidence ../hc_introns_true.gff
     cd ..
 
     cd ..
